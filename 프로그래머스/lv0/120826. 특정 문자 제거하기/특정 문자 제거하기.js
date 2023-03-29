c150cc3598d85bfc40a5accfb2786be804252883
Da-Youn <dayoun202@gmail.com>
2023-03-29 23:10:22 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,9 +1,7 @@
 function solution(my_string, letter) {
     let answer = ''
     for(let i in my_string){
-        if (my_string[i] !== letter){
-             answer += my_string[i];
-        } 
+        answer +=  (my_string[i] !== letter) ? my_string[i] : '';
     }
     return answer;
 }
