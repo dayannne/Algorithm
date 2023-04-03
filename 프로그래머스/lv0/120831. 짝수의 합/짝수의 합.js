84cd3587da563e1d88d0390d471cd61758576fb1
Da-Youn <dayoun202@gmail.com>
2023-04-04 01:06:54 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,8 +1,6 @@
 function solution(n) {
     let answer = 0;
-    for (let i = 0 ; i <= n ; i ++){
-        if(i%2 === 0){
+    for (let i = 2 ; i <= n ; i+= 2){
             answer += i;
-        }
     }return answer
 }
