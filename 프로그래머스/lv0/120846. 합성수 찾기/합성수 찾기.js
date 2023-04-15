63c33fcd4435c25685107827006d49ffc93ade97
Da-Youn <dayoun202@gmail.com>
2023-04-15 23:44:45 +0900

    [level 0] Title: 합성수 찾기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 0
+   for(let i = 1 ; i <= n ; i++){
+       let cnt = 0
+       for(let j = 1 ; j <= i ; j++){
+         if (i%j === 0){
+             cnt++
+         }  
+        }
+       if(cnt >= 3){
+           answer++
+       }
+       
+}return answer}
