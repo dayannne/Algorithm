b77f0cefe7638ad21161c53a7f056e4aa98740cd
Da-Youn <dayoun202@gmail.com>
2023-05-07 20:58:58 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,16 +1,4 @@
 function solution(polynomial) {
-    const arr = polynomial.split(' + ');
-    let xNum = 0
-    let num = 0
-    for(let str of arr){
-        if(str.includes('x')){
-            xNum += str.replace('x','') ? parseInt(str.replace('x','')) : 1
-        }else{
-            num += parseInt(str)
-        }
-    }
-    let answer = []
-    if (xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
-  if (num) answer.push(num);
-    return answer.join(" + ");
+    var answer = polynomial.split(' + ');
+    return answer;
 }
