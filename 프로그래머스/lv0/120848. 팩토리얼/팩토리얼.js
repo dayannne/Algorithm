740e7a73b7b308a96f6598fdc02e6ab92a0853a4
Da-Youn <dayoun202@gmail.com>
2023-05-12 01:34:52 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,12 +1,11 @@
 function solution(n) {
-  let answer = 1;
-  let count = 0;
-  for (let i = 1; i <= 10; i++) {
-    answer *= i;
-    if (answer > n) {
-      break;
+  let num = 1;
+  let fac = 1;
+  function factorial(n, fac, num) {
+    if (fac > n) {
+      return num - 2;
     }
-    count++;
+    return factorial(n, fac * num, num + 1);
   }
-  return count;
+  return factorial(n, fac, num);
 }
