58c279fd5c4c5dcda71528f8f15d9557871ae5e5
Da-Youn <dayoun202@gmail.com>
2023-05-15 01:05:47 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,16 +1,16 @@
-function recursive(a,b,currBottle, result) {
+function recursive(a,b,currBottle, answer) {
   if (currBottle < a) {
-    return result;
+    return answer;
   }
   // '마트에 가져간 빈병'으로 '새로 받은 콜라' 개수
   const newBottle = ~~(currBottle / a) * b;
   // 현재 빈병 개수에서 '마트에 가져간 빈병'을 빼고 '남은 빈병' 개수
   const remainBottle = currBottle % a;
   // currBottle = 새로 받은 콜라 + 남은 빈병 개수를 더해 다시 재귀
-  return recursive(a, b, newBottle + remainBottle, result + newBottle);
+  return recursive(a, b, newBottle + remainBottle, answer + newBottle);
 }
 
 function solution(a, b, n) {
-  let result = 0;
-  return recursive(a, b, n, result);
+  let answer = 0;
+  return recursive(a, b, n, answer);
 }
