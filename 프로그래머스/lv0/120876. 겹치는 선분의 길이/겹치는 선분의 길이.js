7c1bb7e09a748f0cfe2e50fd17ca329c5575a77d
Da-Youn <dayoun202@gmail.com>
2023-05-19 09:19:19 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,19 +1,18 @@
 function solution(lines) {
-  var answer = 0;
-  let lineMap = new Array(200).fill(0);
+  let answer = 0;
+  const linesArr = lines.map(([start, end]) =>
+    Array.from({ length: end - start + 1 }, (_, index) => start + index)
+  );
 
-  for (let i = 0; i < 3; i++) {
-    let left = lines[i][0];
-    let right = lines[i][1];
+  for (let i = 0; i < linesArr.length; i++) {
+    const currentLine = linesArr[i];
 
-    for (let j = left; j < right; j++) {
-      lineMap[j + 100] += 1;
-    }
-  }
+    for (let j = i + 1; j < linesArr.length; j++) {
+      const cntOverlap = linesArr[j].filter(dot => currentLine.includes(dot));
 
-  for (let i in lineMap) {
-    if (lineMap[i] > 1) {
-      answer += 1;
+      if (cntOverlap.length >= 1) {
+        answer++;
+      }
     }
   }
 
