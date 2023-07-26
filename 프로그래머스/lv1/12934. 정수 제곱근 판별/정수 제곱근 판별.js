75cae81b0c541f06b87aebfe91d86c998e39099c
Da-Youn <dayoun202@gmail.com>
2023-07-26 23:07:28 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,4 +1,4 @@
 function solution(n) {
     var answer = Math.sqrt(n);
-    return Number.isInteger(Math.sqrt(n)) ? (answer +1)*(answer +1) : -1;
+    return Number.isInteger(Math.sqrt(n)) ? (answer +1)*(answer +1) : -1; // 제곱표현 -> Math.pow(answer+1,2) 도 가능
 }
