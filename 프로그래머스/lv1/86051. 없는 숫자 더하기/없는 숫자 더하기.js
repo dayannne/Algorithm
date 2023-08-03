f1c0a0b428267fa5033f83fd66348781fd1f7a65
Da-Youn <dayoun202@gmail.com>
2023-08-03 23:17:53 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,4 @@
 function solution(numbers) {
-    const sortedNumbers = numbers.sort((a,b)=> a-b)
-    let answer = 0
-    for(let i = 0 ; i < 10 ; i++){
-        if(sortedNumbers.indexOf(i) === -1){
-           
-            answer +=  i                             
-        }             
-    }         
-    return answer;
+   
+    return 45 - numbers.reduce((a,c) => a + c);
 }
