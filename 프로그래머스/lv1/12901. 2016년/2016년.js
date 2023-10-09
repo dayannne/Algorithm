b23b930ba1142797ab006f13f2bf21c0872181e2
Da-Yeon <dayoun202@gmail.com>
2023-10-09 15:08:48 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,5 +1,16 @@
 function solution(a, b) {
-    const day = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-    let date = new Date(`2016-${a}-${b}`);
-    return day[date.getDay()];
+    // 내 풀이
+    // const day = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    // let date = new Date(`2016-${a}-${b}`);
+    // return day[date.getDay()];
+    
+    // 다른 풀이
+    const monthDay = [31,29,31,30,31,30,31,31,30,31,30,31]
+    const weekDay = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+
+    let days = b
+    for(let i=0 ; i<a-1 ; i++)
+        days += monthDay[i];
+
+    return weekDay[days%7];
 }
