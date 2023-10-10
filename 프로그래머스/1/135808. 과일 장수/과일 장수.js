bad5ed06265e8e8699d67a91c4d83b2f7e82fc89
Da-Yeon <dayoun202@gmail.com>
2023-10-10 09:31:22 +0900

    [level 1] Title: 과일 장수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,25 @@
+function solution(k, m, score) {
+    // 일단 상자는 무조건 k개로 나뉨
+    // 상자 단위로만 판매하고 남는 사과는 버림 
+    // 정렬해서 높은 사과부터 자르기
+    // 사과 분리해주는 함수 만들기
+    const sorted = score.sort((a,b)=>b-a)
+    
+    function chunk (data, size){
+        const arr = []
+        let result = 0
+        for(let i = 0 ; i< data.length; i += size){
+            if(data.slice(i, i+size).length === size)
+            arr.push(data.slice(i, i+size).sort((a,b)=>a-b))
+        }
+                     
+        for(const x of arr){
+            result += x[0] * x.length        
+        }
+        return result
+    }
+    
+    const answer = chunk(sorted, m)
+
+    return answer;
+}
