cae2772add498fb5d662d800503a61fbb6417583
Da-Yeon <dayoun202@gmail.com>
2023-10-31 09:17:54 +0900

    [level 1] Title: 대충 만든 자판, Time: 7.95 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,21 @@
+function solution(keymap, targets) {
+    // keymap에서 순회하여 입력하려는 알파벳의 인덱스가 가장 작은 키를 선택해서 입력
+    // targets를 완성할 수 없으면 -1
+    // target 반복 - keymap 반복 (keymap 내 문자열 인덱스(키번호),문자열 내 알파벳 인덱스(입력횟수))
+    let answer = [];
+    for(const target of targets){
+        let count = 0
+        for(const s of target){
+            // 해당 알파벳이 없는 문자열에는 가장 큰 수를 부여
+            const key = keymap.map(str => str.indexOf(s) > -1 ? str.indexOf(s) + 1 : 10000 )
+            if(!key.some(item => item <= 100)) {
+                count = -1
+                break
+            }
+            else count += Math.min(...key)
+        }
+        answer.push(count)
+    }
+    
+    return answer;
+}
