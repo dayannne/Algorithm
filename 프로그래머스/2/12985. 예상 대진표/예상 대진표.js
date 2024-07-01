dd22323608f2dc72cc7f6a6a7575772bc1036de3
Da-Youn <dayoun202@gmail.com>
2024-07-01 17:14:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b)
+{
+    let count = 0;
+
+    while(true){
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
+        count++
+        if(a === b) break
+
+    }
+    return count;
+}
