3dd5e505d07d036a37674e5ac0e1d4beaca9a69f
Da-Youn <dayoun202@gmail.com>
2024-08-28 11:14:06 +0900

    [Bronze III] Title: 곱셈, Time: 88 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let a = input[0];
+let b = input[1];
+x_1 = b[2]; // 일의 자리
+x_2 = b[1]; // 십의 자리
+x_3 = b[0]; // 백의 자리
+console.log(Number(a) * Number(x_1));
+console.log(Number(a) * Number(x_2));
+console.log(Number(a) * Number(x_3));
+console.log(Number(a) * Number(b));
+
