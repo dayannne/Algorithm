967789b13193ede08e67f590a5adf29024d37030
Da-Youn <dayoun202@gmail.com>
2024-09-03 12:11:50 +0900

    [Bronze II] Title: 수 정렬하기, Time: 96 ms, Memory: 9704 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,15 +1,10 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let n = Number(input[0]);
-let arr = [];
-for (let i = 1; i <= n; i++) { // 한 줄씩 입력받아 배열에 넣기
-arr.push(Number(input[i]));
-}
-arr.sort(function(a, b) { // 오름차순 정렬 수행
-return a - b;
-})
-let answer = "";
-for (let i = 0; i < arr.length; i++) {
-answer += arr[i] + "\n";
-}
-console.log(answer);
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const length = Number(input[0]);
+const sorted_numbers = input
+  .slice(1, length + 1)
+  .map(Number)
+  .sort((a, b) => a - b);
+
+console.log(sorted_numbers.join('\n'));
